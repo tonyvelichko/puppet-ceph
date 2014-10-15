b27a5f58a52c48ebc52586fe9326cebc7539ea7d
Velichko Anton <anton.velichko@me.com>
2014-10-15 19:06:42 +0400

    Update mon.pp
    fixes

--- manifests/profile/mon.pp
+++ manifests/profile/mon.pp
@@ -24,40 +24,6 @@ class ceph::profile::mon {
   ceph::mon { $::hostname:
     authentication_type => $ceph::profile::params::authentication_type,
     key                 => $ceph::profile::params::mon_key,
-    keyring             => $ceph::profile::params::mon_keyring,
-    stage               => 'install'
-  }
-
-  Ceph::Key {
-    inject         => true,
-    inject_as_id   => 'mon.',
-    inject_keyring => "/var/lib/ceph/mon/ceph-${::hostname}/keyring",
-  }
-
-  # this supports providing the key manually
-  if $ceph::profile::params::admin_key {
-    ceph::key { 'client.admin':
-      secret       => $ceph::profile::params::admin_key,
-      cap_mon      => 'allow *',
-      cap_osd      => 'allow *',
-      cap_mds      => 'allow',
-      mode         => $ceph::profile::params::admin_key_mode,
-    }
-  }
-
-  if $ceph::profile::params::bootstrap_osd_key {
-    ceph::key { 'client.bootstrap-osd':
-      secret           => $ceph::profile::params::bootstrap_osd_key,
-      keyring_path     => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
-      cap_mon          => 'allow profile bootstrap-osd',
-    }
-  }
-
-  if $ceph::profile::params::bootstrap_mds_key {
-    ceph::key { 'client.bootstrap-mds':
-      secret           => $ceph::profile::params::bootstrap_mds_key,
-      keyring_path     => '/var/lib/ceph/bootstrap-mds/ceph.keyring',
-      cap_mon          => 'allow profile bootstrap-mds',
-    }
+    keyring             => $ceph::profile::params::mon_keyring
   }
 }
