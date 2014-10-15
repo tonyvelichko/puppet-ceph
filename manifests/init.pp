8708a63a949a9191c45eebc5dafd9df0f54eb7a5
Velichko Anton <anton.velichko@me.com>
2014-10-15 17:15:30 +0400

    Update init.pp
    fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,3 @@
-#   Copyright (C) 2013, 2014 iWeb Technologies Inc.
-#   Copyright (C) 2014 Nine Internet Solutions AG
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-# Author: David Moreau Simard <dmsimard@iweb.com>
-# Author: David Gurtner <aldavud@crimson.ch>
-
 # init takes care of installing/configuring the common dependencies across classes
 # it also takes care of the global configuration values
 ### == Parameters
@@ -137,7 +119,7 @@ class ceph (
       'global/public_network':              value => $public_network;
     }
 
-    if $authentication_type == 'cephx' {
+    /*if $authentication_type == 'cephx' {
       ceph_config {
         'global/auth_cluster_required': value => 'cephx';
         'global/auth_service_required': value => 'cephx';
@@ -151,6 +133,6 @@ class ceph (
         'global/auth_client_required':  value => 'none';
         'global/auth_supported':        value => 'none';
       }
-    }
+    }*/
   }
 }
