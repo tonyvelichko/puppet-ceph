423b99611e16e18b59f88929bc7818fb722f0720
Velichko Anton <anton.velichko@me.com>
2014-10-16 12:40:41 +0400

    Update mon.pp

--- manifests/profile/mon.pp
+++ manifests/profile/mon.pp
@@ -8,8 +8,16 @@ class ceph::profile::mon {
     keyring             => $ceph::profile::params::mon_keyring
   }
 
-  if $admin_key {
+  if $ceph::profile::params::admin_key {
     ceph::key { 'client.admin':
+      secret       => $ceph::profile::params::admin_key,
+      cap_mon      => 'allow *',
+      cap_osd      => 'allow *',
+      cap_mds      => 'allow *',
+      mode         => '0600',
+    }
+    
+    ceph::key { 'client.radosgw.gateway':
       secret       => $ceph::profile::params::admin_key,
       cap_mon      => 'allow *',
       cap_osd      => 'allow *',
@@ -18,7 +26,7 @@ class ceph::profile::mon {
     }
   }
 
-  if $boostrap_osd_key {
+  if $ceph::profile::params::bootstrap_osd_key {
     ceph::key { 'client.bootstrap-osd':
       secret           => $ceph::profile::params::bootstrap_osd_key,
       keyring_path     => '/var/lib/ceph/bootstrap-osd/ceph.keyring',
@@ -26,7 +34,7 @@ class ceph::profile::mon {
     }
   }
 
-  if $boostrap_mds_key {
+  if $ceph::profile::params::boostrap_mds_key {
     ceph::key { 'client.bootstrap-mds':
       secret           => $ceph::profile::params::boostrap_mds_key,
       keyring_path     => '/var/lib/ceph/bootstrap-mds/ceph.keyring',
